4a476db65dd86a9dfbc183357dc051b2d1394342
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-05 22:30:14 -0400

    commit tree 67dff497302ad82e76a2707d857d12800596bf05

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(Pickup) {
+    return Math.abs(Pickup - 42);
+}
+
+function distanceFromHqInFeet(Pickup) {
+    return distanceFromHqInBlocks(Pickup) * 264;
+
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(end - start) * 264;
+}
+
+function calculatesFarePrice(start, end) {
+    let dist = distanceTravelledInFeet(start, end);
+    let price;
+    if (dist >= 400 && dist <= 2000) {
+        price = (dist - 400) * 0.02;
+    }
+    else if (dist > 2000 && dist < 2500) {
+        price = 25;
+    }
+    else if (dist >= 2500) {
+        price = 'cannot travel that far';
+    }
+    else price = 0;
+
+    return price;
+}
